3fab1d7df2d85a033e431a165faa5dae32a61dbd
ZombieWal <walrushere@gmail.com>
2023-05-02 19:44:18 +0300

    added timer in the left corner

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public DroneMovement drone;
+    public TMP_Text timeText;
+
+    void Start()
+    {
+        timerIsRunning = true;    
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+
+            if (drone.isActivated && timeRemaining > 0)
+            {
+                DisplayTime(timeRemaining);
+                timeRemaining -= Time.deltaTime;
+            }
+            else if (drone.isActivated && timeRemaining <= 0)
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+                SceneManager.LoadScene("Fail");
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
